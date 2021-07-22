a8ba67da4750ce4918b43ff5457884e7e827c0c2
Mingwei Samuel <mingwei.samuel@gmail.com>
2021-07-22 04:56:25 +0000

    Add build script

---
+++ build-emscripten.bash
@@ -0,0 +1,33 @@
+set -ex
+
+# Get the emsdk repo
+if [ ! -d "emsdk" ]; then
+    git clone https://github.com/emscripten-core/emsdk.git
+fi
+
+# Enter that directory
+pushd emsdk
+
+# Download and install the latest SDK tools.
+./emsdk install latest
+
+# Make the "latest" SDK "active" for the current user. (writes .emscripten file)
+./emsdk activate latest
+
+# Activate PATH and other environment variables in the current terminal
+source ./emsdk_env.sh
+
+popd
+
+mkdir build || true
+
+pushd build
+
+emcmake cmake -DENABLE_PYTHON_INTERFACE=OFF -DNOM4RI=ON \
+    -DENABLE_TESTING=OFF -DNOZLIB=ON -DONLY_SIMPLE=ON \
+    -DEMSCIPTEN=ON \
+    ..
+
+emmake make
+
+popd
